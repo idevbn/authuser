89900c3117e48100b0b7d1c7718111eb8bab9451
Idevaldo Neto <inetomec@yahoo.com.br>
2024-06-08 11:05:52 -0300

    chore: Removido lançamento de exceções nos métodos
    Também foram reorganizados os imports e removida annotation do Log4j2 de AuthenticationEntryPointImpl

--- src/main/java/com/ead/authuser/configs/security/AccessDeniedHandlerImplement.java
+++ src/main/java/com/ead/authuser/configs/security/AccessDeniedHandlerImplement.java
@@ -1,14 +1,11 @@
 package com.ead.authuser.configs.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class AccessDeniedHandlerImplement implements AccessDeniedHandler {
@@ -18,7 +15,7 @@ public class AccessDeniedHandlerImplement implements AccessDeniedHandler {
             final HttpServletRequest request,
             final HttpServletResponse response,
             final AccessDeniedException accessDeniedException
-    ) throws IOException, ServletException {
+    ) {
         response.setStatus(HttpStatus.FORBIDDEN.value());
     }
 
--- src/main/java/com/ead/authuser/configs/security/AuthenticationEntryPointImpl.java
+++ src/main/java/com/ead/authuser/configs/security/AuthenticationEntryPointImpl.java
@@ -1,17 +1,12 @@
 package com.ead.authuser.configs.security;
 
-import lombok.extern.log4j.Log4j2;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Log4j2
 @Component
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 
@@ -19,7 +14,7 @@ public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
     public void commence(
             final HttpServletRequest httpServletRequest,
             final HttpServletResponse httpServletResponse,
-            final AuthenticationException e) throws IOException, ServletException {
+            final AuthenticationException e) {
         httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
     }
 
